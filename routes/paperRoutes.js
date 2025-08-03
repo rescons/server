beca71ea64f46d47eb5bd5b7658549e555e1f1cf
VishwasPrabhakara <vp14032001@gmail.com>
2025-08-03 12:25:20 +0530

    Updated paper routes

--- routes/paperRoutes.js
+++ routes/paperRoutes.js
@@ -1,22 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const paperController = require('../controllers/paperController');
-const jwt = require('jsonwebtoken');
-
-// Middleware to verify JWT token (same as in server.js)
-const verifyToken = (req, res, next) => {
-  const token = req.header("Authorization")?.replace("Bearer ", "");
-  if (!token) {
-    return res.status(401).json({ message: "No token, authorization denied" });
-  }
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (error) {
-    res.status(401).json({ message: "Token is not valid" });
-  }
-};
+const { verifyToken } = require('../server');
 
 // Apply authentication middleware to all routes
 router.use(verifyToken);
